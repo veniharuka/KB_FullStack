3f62bb627bed0a94d354c11bd96ae0bd821752ca
veniharuka <kurobas1384@gmail.com>
2024-07-17 17:35:29 +0900

    0717 오후 커밋 CLI, SQL

---
+++ 05_Mysql/0717.sql
@@ -0,0 +1,73 @@
+#1번
+USE sqldb;
+CREATE TABLE tbl1(
+
+	a INT PRIMARY KEY,
+    b INT,
+    c INT
+);
+SHOW INDEX FROM tbl1;
+
+#2번
+CREATE TABLE tbl2(
+	a INT PRIMARY KEY,
+    b INT UNIQUE,
+    C INT UNIQUE,
+	d INT
+);
+SHOW INDEX FROM tbl2;
+
+#3번
+CREATE TABLE tbl3(
+	a INT UNIQUE,
+    b INT UNIQUE,
+    C INT UNIQUE,
+	d INT
+);
+SHOW INDEX FROM tbl3;
+
+#4번
+CREATE TABLE tbl4(
+	a INT UNIQUE NOT NULL,
+    b INT UNIQUE,
+    C INT UNIQUE,
+	d INT
+);
+SHOW INDEX FROM tbl4;
+
+#5번
+CREATE TABLE tbl5(
+	a INT UNIQUE NOT NULL,
+    b INT UNIQUE,
+    C INT UNIQUE,
+	d INT PRIMARY KEY
+);
+SHOW INDEX FROM tbl5;
+
+#6번
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+
+DROP TABLE IF EXISTS usertbl;
+CREATE TABLE usertbl(
+	userID CHAR(8) NOT NULL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    birthYear INT NOT NULL,
+    addr NCHAR(2) NOT NULL
+);
+
+ INSERT INTO usertbl VALUES('LSG', '이승기', 1987, '서울');
+ INSERT INTO usertbl VALUES('KBS', '김범수', 1979, '경남');
+ INSERT INTO usertbl VALUES('KKH', '김경호', 1971, '전남');
+ INSERT INTO usertbl VALUES('JYP', '조용필', 1950, '경기');
+ INSERT INTO usertbl VALUES('SSK', '성시경', 1979, '서울'); 
+ 
+ SELECT * FROM usertbl;
+
+
+#7번
+ALTER TABLE usertbl DROP PRIMARY KEY;
+ALTER TABLE usertbl ADD CONSTRAINT pk_name PRIMARY KEY(name);
+
+SELECT *FROM usertbl;
+SHOW INDEX FROM usertbl;
---
+++ 05_Mysql/jdbc_ex/src/main/java/org/scoula/user2/UserDAO.java
@@ -0,0 +1,72 @@
+package org.scoula.user2;
+
+import org.scoula.jdbc_ex.common.JDBCUtil;
+import org.scoula.user2.UserVO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private final Connection conn;
+
+    public UserDAO() {
+        this.conn = JDBCUtil.getConnection();
+    }
+
+    public void addUser(UserVO user) {
+        String sql = "INSERT INTO user_table (userid, name, password, age, membership) VALUES (?,?,?,?,?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUserid());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
+            pstmt.setInt(4, user.getAge());
+            pstmt.setBoolean(5, user.isMembership());
+            pstmt.executeUpdate();
+
+            System.out.println("회원 추가 성공!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<UserVO> getAllUsers() {
+        String sql = "SELECT * FROM user_table";
+        List<UserVO> users = new ArrayList<>();
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                UserVO user =new UserVO(    rs.getInt("id"),
+                        rs.getString("userid"),
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        rs.getInt("age"),
+                        rs.getBoolean("membership"),
+                        rs.getTimestamp("signup_date"));
+
+                users.add(user);
+
+            }
+
+            users.forEach(System.out::println);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users;
+    }
+
+    public void close() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("데이터베이스 연결이 성공적으로 종료되었습니다.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
