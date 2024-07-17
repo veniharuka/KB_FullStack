3896bcf7caf6d9412a7be9f0036d67ca44372809
veniharuka <kurobas1384@gmail.com>
2024-07-17 12:36:02 +0900

    0717 오전 CLI

---
+++ 04_java/CLI_FramWork/untitled/src/ui/App.java
@@ -0,0 +1,28 @@
+package ui;
+
+import command.*;
+
+public abstract class App {
+    Menu menu;
+    public App() {
+    }
+    public void init() {
+        menu = new Menu();
+        createMenu(menu);
+        menu.add(new MenuItem("종료", new ExitCommand()));
+    }
+    public void createMenu(Menu menu) {
+    }
+    public void run() {
+        init();
+        while(true) {
+            menu.printMenu();
+            Command command = menu.getSelect();
+            command.execute();
+        }
+    }
+}
+
+
+
+
