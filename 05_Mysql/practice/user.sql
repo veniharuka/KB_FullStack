8e9658206e7de07c594280c17a68d098f71f6800
veniharuka <kurobas1384@gmail.com>
2024-07-22 20:50:53 +0900

    0722 desktop

---
+++ 05_Mysql/practice/user.sql
@@ -0,0 +1,16 @@
+use user_ex;
+
+CREATE TABLE users{
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email  VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+    };
+
+
+INSERT INTO users (email,password) VALUES {
+    (  'tetz'  ,'1234'),
+    ('siwan',  '1234'),
+    (' na' , '1234')
+
+}
