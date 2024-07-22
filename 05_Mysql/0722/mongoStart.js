d042a20c0d47edb91bad5a5c1a53a515264549cc
veniharuka <kurobas1384@gmail.com>
2024-07-22 15:40:32 +0900

    0722 몽고SQL

---
+++ 05_Mysql/0722/mongoStart.js
@@ -0,0 +1,14 @@
+use my_database;
+db.users.find();
+
+db.users.insert({username : 'smith'});
+
+
+use tutorial;
+db.users.insert({username : 'smith'});
+
+db.users.find();
+db.users.insert({username: "jones"});
+
+db.users.count();
+ db.users.find();
---
+++ CodeStudy/afterStudy/CodeStudy/src/afterStudy/study0719/TargetNumber.java
@@ -0,0 +1,32 @@
+package afterStudy.study0719;
+
+public class TargetNumber {
+
+    int cur=0;
+    int idx=0;
+    public int solution(int[] numbers, int target) {
+        int answer=0;
+        backtrack(0,0,numbers,target);
+        return answer;
+    }
+
+
+    void backtrack(int cur,int idx,int[] numbers,int target) {
+        //basecase
+        if(idx == numbers.length) {
+            if(target == cur) {
+                answer++;
+            }
+        }
+
+        //recursive call
+        backtrack(cur+numbers[idx],idx+1,numbers,target);
+        backtrack(cur-numbers[idx],idx+1,numbers,target);
+
+    }
+    public static void main(String[] args) {
+        TargetNumber t=new TargetNumber();
+        int[] numbers=new int[]{1,2,3,4,5,6,7,8,9};
+        System.out.println(t.solution(numbers,9));
+    }
+}
