c53425fb886fb665c8854fffa3e3c2d7832eff99
veniharuka <kurobas1384@gmail.com>
2024-07-23 17:15:48 +0900

    Create Q05Query.js

---
+++ 05_Mysql/0722/Q05Query.js
@@ -0,0 +1,50 @@
+// 1번
+use test;
+
+for(let i=0; i<20000; i++){
+    db.product.insertOne(
+    
+    {num : i, name: '스마트폰'+i}
+    )
+};
+
+
+//2번 
+db.product.countDocuments();
+
+//3번 +1이면 오름차순 -1이면 내림차순
+db.product.find().sort({num : -1})
+
+//4번
+db.product.find().sort({num : -1}).limit(10);
+
+//5번
+db.product.find().sort({num : -1}).skip(10*5).limit(10);
+//6번
+db.product.find({
+    $or : [
+    {num : {$lt :15}},
+    {num : {$gt :19995}},
+    ]
+});
+//7번
+db.product.find(
+    {
+        name : {
+            $in : ['스마트폰10','스마트폰100','스마트폰1000']
+        }
+    }
+);
+//8번
+db.product.find(
+    { num:{ $lt : 5}   },{_id:0, name :1}
+);
+
+db.product.find();
+
+
+
+
+
+
+
