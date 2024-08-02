ea74119e834c1c96f0899dd3a9c6adea99e24fd4
veniharuka <kurobas1384@gmail.com>
2024-08-02 14:16:59 +0900

    240802 codinTest 수업

---
+++ CodeStudy/afterStudy/CodeStudy/src/afterStudy/study0802/WordSearch.java
@@ -0,0 +1,71 @@
+package afterStudy.study0802;
+
+
+import java.util.*;
+
+public class WordSearch {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        };
+        String word = "ABCCED";
+        String word2 = "ABCD";
+
+        Solution solution = new Solution();
+        boolean result = solution.exist(board, word);
+        boolean result2 = solution.exist(board, word2);
+
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = ' ';  // mark as visited
+
+        boolean visited = dfs(board, word, i + 1, j, index + 1)
+                || dfs(board, word, i - 1, j, index + 1)
+                || dfs(board, word, i, j + 1, index + 1)
+                || dfs(board, word, i, j - 1, index + 1);
+
+        board[i][j] = temp;  // unmark
+
+        return visited;
+    }
+}
+
+
+
+
+
+
+
+
+
