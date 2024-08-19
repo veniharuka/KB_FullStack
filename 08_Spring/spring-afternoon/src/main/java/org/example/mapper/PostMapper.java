1a0ab5f5b43db3dcd7e3ca1fac41e0ce9944b51f
veniharuka <kurobas1384@gmail.com>
2024-08-19 13:18:16 +0900

    240819 게시판 삭제 및 수정 추가

--- 08_Spring/spring-afternoon/src/main/java/org/example/mapper/PostMapper.java
+++ 08_Spring/spring-afternoon/src/main/java/org/example/mapper/PostMapper.java
@@ -8,9 +8,20 @@ import java.util.List;
 
 @Mapper
 public interface PostMapper {
+    // 게시글 전체 조회
     List<PostDto> findAll();
-    List<PostDto> findByCondition(@Param("title") String title,
-                                  @Param("content") String content
+    // 게시글 조건 조회
+    List<PostDto> findByCondition(
+            @Param("title") String title,
+            @Param("content") String content
     );
+    // 게시글을 id 로 조회 -> 게시글 수정에서 사용
+    PostDto findById(@Param("id") Long id);
 
+    // 게시글 삭제
+    int delete(@Param("id") Long id);
+    // 게시글 추가
+    int save(@Param("title") String title, @Param("content") String content);
+    // 게시글 수정
+    int update(@Param("id") Long id, @Param("title") String title, @Param("content") String content);
 }
