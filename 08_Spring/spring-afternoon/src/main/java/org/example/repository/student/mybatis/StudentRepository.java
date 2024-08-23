3693719f2edd8bca4d4f75e2f8eb192fba1512fb
veniharuka <kurobas1384@gmail.com>
2024-08-23 15:30:12 +0900

    studentRepository, config 연결

--- 08_Spring/spring-afternoon/src/main/java/org/example/repository/student/mybatis/StudentRepository.java
+++ 08_Spring/spring-afternoon/src/main/java/org/example/repository/student/mybatis/StudentRepository.java
@@ -1,8 +1,30 @@
 package org.example.repository.student.mybatis;
 
+import org.example.domain.StudentMybatis;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentRepository {
-    
+    private final StudentMapper studentMapper;
+
+    @Autowired
+    public StudentRepository(StudentMapper studentMapper) {
+        this.studentMapper = studentMapper;
+    }
+    public List<StudentMybatis> findAll(){
+        return studentMapper.findAll();
+    }
+
+    public StudentMybatis findById(Long id){
+        return studentMapper.findById(id);
+    }
+    public int save(StudentMybatis student){
+        return studentMapper.save(student);
+    }
+    public int update(StudentMybatis newStudent){
+        return studentMapper.update(newStudent);
+    }
 }
