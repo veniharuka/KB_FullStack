0ebafb1744f24b06bf39501eb06706617865c7ee
veniharuka <kurobas1384@gmail.com>
2024-08-25 16:15:49 +0900

    MybatisStudentController findAll생성

--- 08_Spring/spring-afternoon/src/main/java/org/example/controller/student/MybatisStudentController.java
+++ 08_Spring/spring-afternoon/src/main/java/org/example/controller/student/MybatisStudentController.java
@@ -1,4 +1,30 @@
 package org.example.controller.student;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.StudentMybatis;
+import org.example.repository.student.mybatis.StudentRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/student")
 public class MybatisStudentController {
+    private final StudentRepository studentRepository;
+
+    @GetMapping("/show")
+    public ResponseEntity<List<StudentMybatis>> findAll(){
+        List<StudentMybatis> students = studentRepository.findAll();
+        return ResponseEntity.ok().body(students);
+    }
+
+
 }
