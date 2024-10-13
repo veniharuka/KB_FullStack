6f6cc8b7731c5071989d25c579d277e1e39d587d
veniharuka <kurobas1384@gmail.com>
2024-10-13 15:32:31 +0900

    Blackjack코데

--- CodeStudy/afterStudy/CodeStudy/src/afterStudy/Blackjack.java
+++ CodeStudy/afterStudy/CodeStudy/src/afterStudy/Blackjack.java
@@ -1,31 +1,31 @@
-//package afterStudy;
-//import java.io.*;
-//import java.util.StringTokenizer;
-//
-//
-//
-//public class Blackjack {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//
-//
-//
-//        int N,M = Integer.parseInt(br.readLine());
-//        int [] arr = new int[N];
-//
-//        for (int i = 0; i < N; i++) {
-//            arr[i] = Integer.parseInt(st.nextToken());
-//        }
-//
-//        int result = search(arr,N,M);
-//        System.out.println(result);
-//
-//    }
-//
-//    static int search(int[] arr, int N, int M) {
-//
-//    }
-//
-//
-//}
+package afterStudy;
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+
+public class Blackjack {
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+
+       int N,M = Integer.parseInt(br.readLine());
+       int [] arr = new int[N];
+
+       for (int i = 0; i < N; i++) {
+           arr[i] = Integer.parseInt(st.nextToken());
+       }
+
+       int result = search(arr,N,M);
+       System.out.println(result);
+
+   }
+
+   static int search(int[] arr, int N, int M) {
+
+   }
+
+
+}
